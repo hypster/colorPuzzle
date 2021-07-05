7744ae7e80e6e4e7abfa8ee0e6084afd46e29bbb
hypster <378288304@qq.com>
2021-07-06 00:51:08 +0200

    Updates

---
+++ precache-manifest.843356fc9f709a57ae6181cdf64ee2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e1e3b37dacf8b98ea2fcfdec1fc1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7903a02e7d2120d36a38",
+    "url": "/static/css/main.d64b8655.chunk.css"
+  },
+  {
+    "revision": "f4983d2ea8b9c149956d",
+    "url": "/static/js/2.de55da19.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.de55da19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903a02e7d2120d36a38",
+    "url": "/static/js/main.0745215e.chunk.js"
+  },
+  {
+    "revision": "c95bd1d98190eefe239b",
+    "url": "/static/js/runtime-main.6616f004.js"
+  }
+]);
